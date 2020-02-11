7f3bd67ee683b718be713a1fbcfcbd4500510775
krlex <krle@tilda.center>
2020-02-11 05:54:16 +0100

    create new Vagrantfile config and add bin/common.sh

--- bin/common.sh
+++ bin/common.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-sudo apt install -y curl git python3-pip python3-venv tmux
-update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
 BIN_DIR="/vagrant/bin"
+echo "Starting installation common packages"
+sudo apt install -y curl git python3-pip python3-venv tmux >/dev/null 2>&1
+echo "Setup python3 as a python default"
+update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
 
-curl -sL https://deb.nodesource.com/setup_13.x | bash -
+echo "Setup and download nodejs packages"
+curl -sL https://deb.nodesource.com/setup_13.x | bash - >/dev/null 2>&1
+sudo apt update > /dev/null 2>&1
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install nodejs -y
+echo "Starting installation "
+sudo apt install nodejs -y > /dev/null 2>&1
 
-$BIN_DIR/download_repos.sh
-$BIN_DIR/devel.sh
+echo "$BIN_DIR/download_repos.sh" > /home/vagrant/start.sh > /dev/null 2>&1
+echo "$BIN_DIR/devel.sh" >> /home/vagrant/start.sh > /dev/null 2>&1
+chmod +x /home/vagrant/start.sh > /dev/null 2>&1
+
+echo "You need to start init script"
+echo "Example: \n
+      vagrant ssh \n
+      ./init.sh"
